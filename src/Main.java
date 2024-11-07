d5658ad37647a02690edfac9dd25eb5e525ea63f
Ada438 <dolinskaia-alina@mail.ru>
2024-11-08 00:52:41 +0300

    HW-5

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        var clientOS = 1;
-        var clientDeviceYear = 2018;
+        var clientOS = 0;
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+        } else
+            System.out.println("Установите версию приложения для Android по ссылке");
+
+
+
+        var clientDeviceYear = 2013;
+
         if (clientOS == 0) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            if (clientDeviceYear<2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
             } else System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
             if (clientDeviceYear < 2015) {
@@ -17,51 +25,57 @@ public class Main {
 
 
         int year = 2021;
-        if (year % 4 == 0 && year % 100 != 0 && year > 1584) {
+        if (year > 1584) {
+            if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
             System.out.println("Год високосный");
-        } else System.out.println("Год не високосный");
+        }   else
+            System.out.println("Год не високосный");
+        }
+         else System.out.println ("Год не может быть меньше 1584");
 
 
-        var deliveryDistance = 95;
-        var deliveryTime = 0;
-        if (deliveryDistance > 100) {
-            System.out.println("Доставки нет");
-        } else {
-            if (deliveryDistance <= 20) {
-                deliveryTime = 1;
-            } else if (deliveryDistance <= 60) {
-                deliveryTime = 2;
-            } else deliveryTime = 3;
+            var deliveryDistance = 95;
+            var deliveryTime = 1;
+            if (deliveryDistance > 100) {
+                System.out.println("Доставки нет");
+            } else if (deliveryDistance > 20) {
+                deliveryTime++;
+            }
+            if (deliveryDistance > 60) {
+                deliveryTime++;
+            }
             System.out.println("Потребуется дней: " + deliveryTime);
+
+
+            var monthNumber = 12;
+            switch (monthNumber) {
+                case 1:
+                case 2:
+                case 12:
+                    System.out.println("Зима");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("Весна");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println("Лето");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println("Осень");
+                    break;
+                default:
+                    System.out.println("Такого месяца не существует");
+            }
         }
-
-        var monthNumber = 12;
-        switch (monthNumber) {
-            case 1:
-            case 2:
-            case 12:
-                System.out.println("Зима");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("Весна");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("Лето");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("Осень");
-                break;
-            default:
-                System.out.println("Такого месяца не существует");
-        }
-
-
     }
-}
+
+
+
+
 
