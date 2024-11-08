67f3bce3106f23f243b3ee0d62ed1ac09d4432d0
Ada438 <dolinskaia-alina@mail.ru>
2024-11-08 12:24:21 +0300

    HW-5

--- src/Main.java
+++ src/Main.java
@@ -3,48 +3,52 @@
 public class Main {
     public static void main(String[] args) {
 
-        var clientOS = 0;
-            if (clientOS == 0) {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-        } else
-            System.out.println("Установите версию приложения для Android по ссылке");
-
-
-
-        var clientDeviceYear = 2013;
-
+        var clientOS = 3;
         if (clientOS == 0) {
-            if (clientDeviceYear<2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else System.out.println("Установите версию приложения для iOS по ссылке");
-        } else {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else System.out.println("Установите версию приложения для Android по ссылке");
-        }
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");}
+        else
+            System.out.println("Такой ОС нет");
 
 
-        int year = 2021;
-        if (year > 1584) {
-            if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
-            System.out.println("Год високосный");
-        }   else
-            System.out.println("Год не високосный");
-        }
-         else System.out.println ("Год не может быть меньше 1584");
+            var clientDeviceYear = 2013;
+        if (clientOS != 0 && clientOS != 1) {
+            System.out.println("Такой ОС нет");
+        } else if (clientOS == 0) {
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                } else System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                if (clientDeviceYear < 2015 && (clientOS ==1 || clientOS ==0)) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                } else System.out.println("Установите версию приложения для Android по ссылке");
+            }
 
 
-            var deliveryDistance = 95;
+
+            int year = 2021;
+            if (year > 1584) {
+                if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+                    System.out.println("Год високосный");
+                } else
+                    System.out.println("Год не високосный");
+            } else System.out.println("Год не может быть меньше 1584");
+
+
+            var deliveryDistance = 105;
             var deliveryTime = 1;
             if (deliveryDistance > 100) {
                 System.out.println("Доставки нет");
-            } else if (deliveryDistance > 20) {
-                deliveryTime++;
+            } else {
+                if (deliveryDistance > 20) {
+                    deliveryTime++;
+                }
+                if (deliveryDistance > 60) {
+                    deliveryTime++;
+                }
+                System.out.println("Потребуется дней: " + deliveryTime);
             }
-            if (deliveryDistance > 60) {
-                deliveryTime++;
-            }
-            System.out.println("Потребуется дней: " + deliveryTime);
 
 
             var monthNumber = 12;
@@ -73,6 +77,7 @@ public class Main {
                     System.out.println("Такого месяца не существует");
             }
         }
+
     }
 
 
